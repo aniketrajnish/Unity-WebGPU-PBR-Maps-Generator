834071f35faa950c31f4b3767a22e977a00f428d
Aniket Rajnish <58925008+aniketrajnish@users.noreply.github.com>
2024-08-04 13:46:40 -0400

    [Update] WebGPU GUI Check #1

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "makra-PBR Maps Generator-0.1";
 const contentToCache = [
-    "Build/gpu_good_build.loader.js",
-    "Build/gpu_good_build.framework.js",
-    "Build/gpu_good_build.data",
-    "Build/gpu_good_build.wasm",
+    "Build/gpu_build_3.loader.js",
+    "Build/gpu_build_3.framework.js",
+    "Build/gpu_build_3.data",
+    "Build/gpu_build_3.wasm",
     "TemplateData/style.css"
 
 ];
